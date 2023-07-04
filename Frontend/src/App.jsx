e3061604ae1b47bbebf4ac021dc625a9501fa9f0
V Suhas <suhas3145@gmail.com>
2023-07-04 16:15:54 +0530

    Basic react-router-dom setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,15 +4,47 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-
+import ProductList from "./pages/ProductList";
+import{
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 const App = () =>{
-    return <>
-    <Login/>
-    <Cart/>
-    <Home/>
-    </>;
+    const user = true;
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+
+                <Route path="/Products/:category">
+                    <ProductList/>
+                </Route>
+
+                <Route path="/Product/:id">
+                    <Product/>
+                </Route>
+
+                <Route path="/cart">
+                    <Cart/>
+                </Route>
+
+                <Route path="/login">
+                    {user ? <Redirect to= "/"/> : <Login/>}
+                </Route>
+
+                <Route path="/register">
+                    {user ? <Redirect to= "/"/> : <Register/>}
+
+                </Route>
+
+            </Switch>
+        </Router>
+    );
 };
 
 export default App;
