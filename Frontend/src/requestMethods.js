6ca60b963cbd151dc81f7849b786f39d74770810
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-07-23 14:36:12 +0530

    add admin_page

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:3000/api/";
-const TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OWVkOTM3MTEyYTNiNjllY2U3ODNmMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4OTc4MTAxNCwiZXhwIjoxNjkwMDQwMjE0fQ.zJ8Rr_cAIrAan49CRbw4QWCAXyM6fOvU5pvkqI2ezkw";
+const TOKEN =JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
-  });
+  baseURL: BASE_URL,
+});
 
 export const userRequest = axios.create({
-    baseURL: BASE_URL,
-    header: { token: `Bearer ${TOKEN}` },
-  });
+  baseURL: BASE_URL,
+  header: { token: `Bearer ${TOKEN}` },
+});
