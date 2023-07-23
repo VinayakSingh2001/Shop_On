93997fe02f043791f7367d291f9ead528e075cef
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-07-24 00:41:42 +0530

    add firebase storage upload feature

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAq3TTrvFsh5u3zmJBjmwk_CtBPJDxDIm0",
+  authDomain: "shop-69c94.firebaseapp.com",
+  projectId: "shop-69c94",
+  storageBucket: "shop-69c94.appspot.com",
+  messagingSenderId: "644010083384",
+  appId: "1:644010083384:web:37f61a83fc0ba1e5ce52cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
